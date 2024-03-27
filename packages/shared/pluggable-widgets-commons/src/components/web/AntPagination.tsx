bd4d5201fa8b81cb2f019b1b21ee1c14469afeaf
engalar <engalar@gmail.com>
2024-03-27 11:55:17 +0800

    fix(datagrid): local

--- packages/shared/pluggable-widgets-commons/src/components/web/AntPagination.tsx
+++ packages/shared/pluggable-widgets-commons/src/components/web/AntPagination.tsx
@@ -38,6 +38,20 @@ export function AntPagination(props: AntPaginationProps): ReactElement | null {
     return (
         <div aria-label="Pagination" className="pagination-bar" role="pagination">
             <Pagination
+                locale={{
+                    // Options.jsx
+                    items_per_page: "条/页",
+                    jump_to: "跳至",
+                    jump_to_confirm: "确定",
+                    page: "页",
+                    // Pagination.jsx
+                    prev_page: "上一页",
+                    next_page: "下一页",
+                    prev_5: "向前 5 页",
+                    next_5: "向后 5 页",
+                    prev_3: "向前 3 页",
+                    next_3: "向后 3 页"
+                }}
                 showQuickJumper
                 showSizeChanger
                 pageSizeOptions={[5, 10, 20, 50, 100]}
